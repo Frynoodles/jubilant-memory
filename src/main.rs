2da755f103ad5dc85650f0e05eac181a5d967e63
Frynoodles <sanshuidesu@163.com>
2023-03-18 20:52:15 +0800

    add 404 file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let request = String::from_utf8_lossy(&buffer[..]);
+    let response = if request.starts_with("GET") {
+        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, world!"
+    } else {
+        "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\n\r\nInvalid Request"
+    };
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(|| {
+                    handle_client(stream);
+                });
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
 }
